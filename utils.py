06d3d173b7267c07699108459287b28bf3081d13
Bharath2004-M <bharathofficiall007@gmail.com>
2025-05-21 18:41:22 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 # utils.py
 import requests
 
-TMDB_API_KEY = 'YOUR_TMDB_API_KEY'  # Replace with your actual TMDB API key
+TMDB_API_KEY = '4e3ac53e6711051a3a2d9a519d3ad868'  # Replace with your actual TMDB API key
 
 def fetch_poster(movie_id):
     url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={TMDB_API_KEY}&language=en-US"
